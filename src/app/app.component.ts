9a2ba3827d043850c28ef2466aecffa89b2cf42d
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-01 18:30:47 +0530

    front-end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'corona';
+
+  url = 'https://hpb.health.gov.lk/api/get-current-statistical?fbclid=IwAR1eafcuQN781zHjhEutTsE06B40gkoa1L4k3TgM2yCWrAr4OZW8LGoGhGo';
+  latest_updated_time = "";
+  constructor(private http: HttpClient) {
+    this.http.get(this.url).toPromise().then(data => {
+      console.log(data);
+      this.latest_updated_time=data["data"].update_date_time;
+    })
+  }
+
 }
