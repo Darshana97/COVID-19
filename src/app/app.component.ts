43dc332bcb1e400c60c4b40de3ed99eea82a468b
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-02 09:40:37 +0530

    designing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,40 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
 
   url = 'https://hpb.health.gov.lk/api/get-current-statistical?fbclid=IwAR1eafcuQN781zHjhEutTsE06B40gkoa1L4k3TgM2yCWrAr4OZW8LGoGhGo';
+
   latest_updated_time = "";
+  local_info_data = "";
+  local_total = "";
+  local_individual_hospital = "";
+  local_death_people = "";
+  local_new_death_people = "";
+  local_recovered_people = "";
+  local_active_cases_people = "";
+  global_new_cases_people = "";
+  global_total_cases_people = "";
+  global_deaths_people = "";
+  global_new_deaths_people = "";
+  global_recovered_people = "";
+
   constructor(private http: HttpClient) {
+
     this.http.get(this.url).toPromise().then(data => {
+
       console.log(data);
-      this.latest_updated_time=data["data"].update_date_time;
+      this.latest_updated_time = data["data"].update_date_time;
+      this.local_info_data = data["data"].local_new_cases;
+      this.local_total = data["data"].local_total_cases;
+      this.local_individual_hospital = data["data"].local_total_number_of_individuals_in_hospitals;
+      this.local_death_people = data["data"].local_deaths;
+      this.local_new_death_people = data["data"].local_new_deaths;
+      this.local_recovered_people = data["data"].local_recovered;
+      this.local_active_cases_people = data["data"].local_active_cases;
+      this.global_new_cases_people = data["data"].global_new_cases;
+      this.global_total_cases_people = data["data"].global_total_cases;
+      this.global_deaths_people = data["data"].global_deaths;
+      this.global_new_deaths_people = data["data"].global_new_deaths;
+      this.global_recovered_people = data["data"].global_recovered;
+
     })
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import {MatButtonModule} from '@angular/material/button';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LocalComponent } from './local/local.component';
+import { GlobalComponent } from './global/global.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LocalComponent,
+    GlobalComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
